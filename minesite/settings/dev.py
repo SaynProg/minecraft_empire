fe3317d8e8e12c9af6c6c78f21e778e8a4d78fa7
SaynProg <103896360+SaynProg@users.noreply.github.com>
2022-11-26 10:59:23 +0200

    Update dev.py

--- minesite/settings/dev.py
+++ minesite/settings/dev.py
@@ -4,7 +4,7 @@ from .base import *
 DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!1al421uvg1qi4-+bh@-*gquxc^jhheu)g^0^6))@15x-cdk09"
+SECRET_KEY = "###"
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
